3fed0def0c71a1df11df10e6da1837defc69e01e
molllol <pogmog7@gmail.com>
2024-02-16 13:06:47 +0200

    Code updates

--- Shop-api/index.js
+++ Shop-api/index.js
@@ -5,7 +5,6 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
-const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe");
 
 
@@ -24,7 +23,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
-app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
 
